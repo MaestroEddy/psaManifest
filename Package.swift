0216bc567c45904b8ff37e74dd142207698e2b5d
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-27 12:07:32 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(name: "PSACommon",
                       platforms: [.iOS(.v10)],
                       products: [.library(name: "PSACommon", targets: ["PSACommon"])],
                       targets: [.binaryTarget(name: "PSACommon", 
-                                              url: "https://github.com/MaestroEddy/PSACommonPackage/tree/main/PSACommon.xcframework", 
+                                              url: "https://github.com/MaestroEddy/PSACommonPackage/blob/main/PSACommon.xcframework.zip", 
                                               checksum: "bfabff6827a014fa34f6496f59b06bea0480f163bc41fc5145fdc9defce5436c")]
                      )
