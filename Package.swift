490b391a9d053fae11df09c34df68d6de1855dc4
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-27 18:42:09 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(name: "PSA",
                       platforms: [.iOS(.v10)],
                       products: [.library(name: "PSA", targets: ["PSACommon", "ePaymentsUI", "PSA"])],
-                      targets: [.target(name: "PSA"),
+                      targets: [//.target(name: "Okay"),
                                 .binaryTarget(name: "PSACommon", 
                                               url: "https://github.com/MaestroEddy/PSACommonPackage/raw/df6058e1ab232104ae1f9f9ad328de7f9acdf37b/PSACommon.xcframework.zip", 
                                               checksum: "bfabff6827a014fa34f6496f59b06bea0480f163bc41fc5145fdc9defce5436c"),
