47c3c4e8b3c6231e5562c1fc4e2bf4ce28b1c9a5
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-27 17:34:54 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(name: "PSACommon",
                                               checksum: "bfabff6827a014fa34f6496f59b06bea0480f163bc41fc5145fdc9defce5436c"),
                                 .binaryTarget(name: "ePaymentsUI", 
                                               url: "https://github.com/MaestroEddy/PSACommonPackage/raw/abe47e4bde40d58795822ac1b1d3cba1596ae276/ePaymentsUI.xcframework.zip", 
-                                              checksum: "bfabff6827a014fa34f6496f59b06bea0480f163bc41fc5145fdc9defce5436c")
+                                              checksum: "1b95bc6a3415b5905d290a56e036bae426c6c75bd4dcc2cd672e09bb5b3f7ab0")
                                ]
                      )
